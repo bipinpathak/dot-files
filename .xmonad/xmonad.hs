96d67cde0ddfa18ce2b0d30c02a2e05f292a7231
Bipin Pathak <bipinpathak1411@gmail.com>
2021-12-23 20:52:53 +0530

    added keymap for flameshot and set default editor

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -70,6 +70,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "dmenu_run")
 
+    -- launch flameshot
+    , ((modm,               xK_s     ), spawn "flameshot gui")
+
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
 
